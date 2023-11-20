baf2bbbb9625fefd4acc940750f5ac5ca04a6807
Carson127 <149115477+Carson127@users.noreply.github.com>
2023-11-19 20:21:24 -0600

    Update plot_of_ice.py

--- Climate Change Projcet/Ice Levels Data/plot_of_ice.py
+++ Climate Change Projcet/Ice Levels Data/plot_of_ice.py
@@ -10,13 +10,12 @@ df = df.drop(columns=df.columns[2:])
 df = df.loc[3612:6473:365]
 
 sqkm_to_sqmiles = 0.239913
-df['(1) Northern_Hemisphere'] = df['(1) Northern_Hemisphere'] * sqkm_to_sqmiles
+df['Northern_Hemisphere'] = df['Northern_Hemisphere'] * sqkm_to_sqmiles
 
-plt.xticks(df['(0) Date'], ['2016','2017','2018','2019','2020','2021', '2022', '2023'])
+plt.xticks(df['Date'], ['2016','2017','2018','2019','2020','2021', '2022', '2023'])
 
-plt.plot(df['(0) Date'], df['(1) Northern_Hemisphere'], label = 'Northern_Hemisphere')
+plt.plot(df['Date'], df['Northern_Hemisphere'], label = 'Northern_Hemisphere')
 
 plt.title('Ice Levels In The Northern Hemisphere')
 plt.ylabel('Square miles of ice')
-plt.legend()
 plt.show()
